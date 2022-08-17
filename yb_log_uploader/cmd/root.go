b69406d7d8130b8e2cc9ec998514eeb13779c441
Craig Goldstein <cgoldstein@yugabyte.com>
2022-08-17 19:03:12 -0400

    WIP: working wireframe with sendsafely sdk loaded, cobra commands, and logging

---
+++ yb_log_uploader/cmd/root.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"main/log"
+	"main/uploader"
+	"os"
+)
+
+var logger = log.Log()
+
+var caseNumFlag int
+var emailFlag string
+var dropzoneIdFlag string
+
+var rootCmd = &cobra.Command{
+	Use:   "yb_log_uploader",
+	Short: "utility to upload logs to yugabyte support",
+	Run: func(cmd *cobra.Command, args []string) {
+		isDropzoneFlagChanged := cmd.Flags().Changed("dropzone_id")
+		uploader.UploadLogs(caseNumFlag, emailFlag, dropzoneIdFlag, isDropzoneFlagChanged)
+	},
+}
+
+func Execute() {
+
+	if err := rootCmd.Execute(); err != nil {
+		_, err := fmt.Fprintln(os.Stderr, err)
+		if err != nil {
+			return
+		}
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().IntVarP(&caseNumFlag, "case_num", "c", 0, "Zendesk case number to attach files to (required)")
+	rootCmd.Flags().StringVarP(&emailFlag, "email", "e", "", "Email address of submitter (required)")
+	rootCmd.Flags().StringVar(&dropzoneIdFlag, "dropzone_id", "S4dsLt2meOtq1iWgBhqJYsuEe2nzvYuv03j_Y6LqhY0", "Override default dropzone ID")
+
+	rootCmd.MarkFlagRequired("case_num")
+	rootCmd.MarkFlagRequired("email")
+	rootCmd.Flags().MarkHidden("dropzone_id")
+}
---
+++ yb_log_uploader/uploader/uploader.go
@@ -0,0 +1,11 @@
+package uploader
+
+import (
+	"main/log"
+)
+
+var logger = log.Log()
+
+func UploadLogs(caseNum int, email string, dropzoneId string, isDropzoneFlagChanged bool) {
+	// code to upload logs to SendSafely
+}
