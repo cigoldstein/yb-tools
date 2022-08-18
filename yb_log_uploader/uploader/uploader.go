a7b382ee6325b6d2eec8a10ed13eda592c44e6fa
Craig Goldstein <cgoldstein@yugabyte.com>
2022-08-18 11:34:53 -0400

    WIP: first API call to create package complete

--- yb_log_uploader/uploader/uploader.go
+++ yb_log_uploader/uploader/uploader.go
@@ -1,11 +1,39 @@
 package uploader
 
 import (
+	"fmt"
+	"io/ioutil"
 	"main/log"
+	"net/http"
 )
 
 var logger = log.Log()
 
+func createDropzonePackage(dropzoneId string) {
+
+	url := "https://secure-upload.yugabyte.com/drop-zone/v2.0/package/"
+	ssApiKeyHeader := dropzoneId
+	ssRequestApiHeader := "DROP_ZONE"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, url, nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("ss-api-key", ssApiKeyHeader)
+	req.Header.Set("ss-request-api", ssRequestApiHeader)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	logger.Info(resp.StatusCode)
+	fmt.Println(string(body))
+
+}
+
 func UploadLogs(caseNum int, email string, dropzoneId string, isDropzoneFlagChanged bool) {
-	// code to upload logs to SendSafely
+	createDropzonePackage(dropzoneId)
 }
